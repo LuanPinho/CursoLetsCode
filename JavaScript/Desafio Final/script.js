8a715998a9fcce14e0efd3e7e2bb5e8afc8868c7
Luan Pinho <luanpinho20@gmail.com>
2022-03-11 20:21:26 -0300

    Update script.js

--- JavaScript/Desafio Final/script.js
+++ JavaScript/Desafio Final/script.js
@@ -1,33 +1,55 @@
 let item = 1;
-/*const marca = document.getElementById('marca').value;
-const modelo= document.getElementById('modelo').value;
-const ano= document.getElementById('ano').value;
-const cor= document.getElementById('cor').value;
-const placa= document.getElementById('placa').value;
-const tipo= document.getElementById('tipo').value;
-const valor= document.getElementById('valor').value*/
+let carros = [];
 
-const carro = {
-    marca: document.getElementById('marca').value,
-    modelo: document.getElementById('modelo').value,
-    ano: document.getElementById('ano').value,
-    cor: document.getElementById('cor').value,
-    placa: document.getElementById('placa').value,
-    tipo: document.getElementById('tipo').value,
-    valor: document.getElementById('valor').value
+let marca = document.getElementById('marca').value;
+let modelo = document.getElementById('modelo').value;
+let ano = document.getElementById('ano').value;
+let cor = document.getElementById('cor').value;
+let placa = document.getElementById('placa').value;
+let tipo = document.getElementById('tipo').value;
+let valor = document.getElementById('valor').value;
+let lista = document.getElementById("lista").value;
+
+function carro(item,marca,modelo,ano,cor,placa,tipo,valor) {
+    this.item = item;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.cor = cor;
+    this.placa = placa;
+    this.tipo = tipo;
+    this.valor = valor
 }
+
 function cadastrar() {
+    let carro1 = new carro (item, marca.value, modelo, ano, cor, placa, tipo, valor);
     
-    console.log(carro);
-    item = item++;
+    carros.push(carro1);
+    lista.innerHTML = carros.map(function(carro1){
+        return `<tr>
+            <td>${carro1.item}</td>
+            <td>${carro1.marca}</td>
+            <td>${carro1.modelo}</td>
+            <td>${carro1.ano}</td>
+            <td>${carro1.cor}</td>
+            <td>${carro1.placa}</td>
+            <td>${carro1.tipo}</td>
+            <td>${carro1.valor}</td>
+        </tr>`
+    }).join("");
+    console.log(carros)
+    item++;
+
 }
+
 function limpar() {
-    carro.marca = 'asad';
-    carro.modelo = '';
-    carro.ano = '';
-    carro.cor = '';
-    carro.placa = '';
-    carro.tipo = 'zero';
-    carro.valor = '';
-    console.log(carro)
+    marca.innerText = '';
+    modelo.innerText = '';
+    ano.innerText = '';
+    cor.innerText = '';
+    placa.innerText = '';
+    tipo.innerText = '';
+    valor.innerText = '';
+   
+    console.log(marca.value)
 }
