17b1749106c17f5bbe761980f7a7ad74e424dec1
Luan Pinho <luanpinho20@gmail.com>
2022-04-11 19:20:51 -0300

    a

---
+++ React/20220406/app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Count from './Components/Count/index'
+import MostrarCount from "./Components/Count/mostrarCount";
+
+function App() {
+  return (
+    <div className="App">
+        <Count/>
+        <hr/>
+        <MostrarCount/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/20220406/app/src/Components/Count/mostrarCount.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react"
+
+export default function MostrarCount(count) {
+    const [count,setCount] = useState(0)
+    
+    return <>
+        <h1>Contador: {count}</h1>
+        <button onClick={() => setCount(count + 1)}>Me click</button>
+    </>
+}
